3878736dbb297033e71a95a97233f583bde301e7
luka shinjiakshvili <114741110+lukapain@users.noreply.github.com>
2023-05-04 17:55:02 +0400

    added delate,pagination logic

--- frontend/src/scenes/Listing.tsx
+++ frontend/src/scenes/Listing.tsx
@@ -1,7 +1,75 @@
-import React from 'react'
+import { log } from 'console';
+import React, { useEffect, useState } from 'react'
 import { Link } from 'react-router-dom'
-
+type User = {
+    id: number;
+    name: string;
+    location: string;
+    price: number;
+  }
 export default function Listing() {
+    const [users, setUsers] = useState<User[]>([]);
+    const [currentPage, setCurrentPage] = useState(1);
+    const [itemsPerPage, setItemsPerPage] = useState(21);
+  
+    useEffect(() => {
+      fetch('http://localhost:8080/users')
+        .then((response) => response.json())
+        .then((data) => setUsers(data))
+        .catch((error) => console.error(error));
+    }, []);
+    {/**ცხრილში ბოლო ნივთის ინდექსი,ქვემოთ კი პირველი. */}
+    const indexOfLastItem = currentPage * itemsPerPage;
+    const indexOfFirstItem = indexOfLastItem - itemsPerPage;
+    const currentItems = users.slice(indexOfFirstItem, indexOfLastItem);
+   
+    
+    {/**წაშლის ლოგიკა*/}
+     const delateUser = (id:number) => {
+        fetch(`http://localhost:8080/users/${id}`, {
+            method:'DELETE'
+        }).then((response) => response.json())
+        .then((data) => {
+            setUsers(users.filter((user) => user.id !==id))
+        }).catch((error) => console.log(error))
+    }
+    {/**ეს იმიტომ გავაკეთე რომ შეგვეძლოს კოდის გამარტივება და პატარა ნაწილებად დაყოფა,ადვილად მენეჯმენტისთვის. */}
+    const renderTableData = currentItems.map((user, index) => {
+      return (
+        <tr key={index}>
+          <td>{user.name}</td>
+          <td>{user.location}</td>
+          <td>{user.price}</td>
+          <td>
+           
+            <button className='btn btn-danger' onClick={() => delateUser(user.id)}>
+              Delete
+            </button>
+          </td>
+        </tr>
+      );
+    });
+    {/** ამ ფუნქცისს გამოყენებით შეგვიძლია დავთვალოთ და გავიგოთ თუ რომელ გვერძე ვართ.ლოგიკა მარტივია და ადვილი გამოსაყენებელი.*/}
+    const renderPageNumbers = () => {
+      const pageNumbers = [];
+      for (let i = 1; i <= Math.ceil(users.length / itemsPerPage); i++) {
+        pageNumbers.push(
+          <li
+            key={i}
+            className={`page-item ${currentPage === i ? 'active' : ''}`}
+            onClick={() => setCurrentPage(i)}
+          >
+            <a href='!#' className='page-link'>
+              {i}
+            </a>
+          </li>
+        );
+      }
+      return pageNumbers;
+    };
+
+  
+
   return (
     <div className='container'>
         <div className='card'>
@@ -23,15 +91,13 @@ management</h2>
                     </tr>
                 </thead>
                 <tbody>
-                    {/**api fetch for get data*/}
-
-
-
-
-
-
+                    {/**კოდის გამარტივებისთვის,ასევე შეგვეძლოს refactor.*/}
+                    {renderTableData}
                 </tbody>
                 </table>
+                <nav aria-label='Page navigation'>
+            <ul className='pagination'>{renderPageNumbers()}</ul>
+          </nav>
             </div>
         </div>
 
