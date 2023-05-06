079669d56c977475a35c28952050c9c3802306ab
luka shinjiakshvili <114741110+lukapain@users.noreply.github.com>
2023-05-06 15:04:12 +0400

    made spinner for loading.

---
+++ frontend/src/components/Spinner.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Spinner() {
+  return (
+    <div className="bouncing-loader d-flex align-items-center justify-content-center vh-100 ">
+    <div></div>
+    <div></div>
+    <div></div>
+  </div>
+  )
+}
