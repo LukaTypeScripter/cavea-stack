c71d309417b6c1768008bff50f61f766bc485e49
luka shinjiakshvili <114741110+lukapain@users.noreply.github.com>
2023-05-06 15:06:13 +0400

    added sorting,ItemsLeft logic.

--- frontend/src/scenes/Listing.tsx
+++ frontend/src/scenes/Listing.tsx
@@ -3,6 +3,7 @@ import React, { useEffect, useState } from "react";
 import { Modal, Button } from "react-bootstrap";
 import { Link } from "react-router-dom";
 import ModalShow from "../components/Modal";
+import Spinner from "../components/Spinner";
 
 type User = {
   id: number;
@@ -10,6 +11,7 @@ type User = {
   location: string;
   price: number;
 };
+
 export default function Listing() {
   const [users, setUsers] = useState<User[]>([]);
   const [currentPage, setCurrentPage] = useState(1);
@@ -20,26 +22,30 @@ export default function Listing() {
 const items = users.length
 
 useEffect(() => {
-  async function fetchData() {
-    setIsLoading(true); 
-    try {
-      const response = await fetch('http://localhost:8080/users');
-      const data = await response.json();
+  setIsLoading(true);
+  fetch(`/Inventories?page=${currentPage}&limit=${itemsPerPage}`)
+    .then((res) => res.json())
+    .then((data) => {
+      data.sort((a: User, b: User) => {
+        const nameCompare = a.name.localeCompare(b.name);
+        if (nameCompare !== 0) {
+          return nameCompare;
+        }
+        const locationCompare = a.location.localeCompare(b.location);
+        if (locationCompare !== 0) {
+          return locationCompare;
+        }
+        return a.price - b.price;
+      });
       setUsers(data);
-      setIsLoading(false); 
-    } catch (error) {
-      console.error(error);
-    }
-  }
-  fetchData();
+      setIsLoading(false);
+    })
+    .catch((err) => console.log(err));
 }, []);
 
-  {
-    /**ცხრილში ბოლო ნივთის ინდექსი,ქვემოთ კი პირველი. */
-  }
-  const indexOfLastItem = currentPage * itemsPerPage;
-  const indexOfFirstItem = indexOfLastItem - itemsPerPage;
-  const currentItems = users.slice(indexOfFirstItem, indexOfLastItem);
+
+
+
   function handleDeleteClick(id: number) {
     setUserIdToDelete(id);
     setShowModal(true);
@@ -49,7 +55,7 @@ useEffect(() => {
     /**წაშლის ლოგიკა*/
   }
   const deleteUser = (id: number) => {
-    fetch(`http://localhost:8080/users/${id}`, {
+    fetch(`http://localhost:8080/Inventories/${id}`, {
       method: "DELETE",
     })
       .then((response) => response.json())
@@ -65,12 +71,11 @@ useEffect(() => {
     /** ამ ფუნქცისს გამოყენებით შეგვიძლია დავთვალოთ და გავიგოთ თუ რომელ გვერძე ვართ.ლოგიკა მარტივია და ადვილი გამოსაყენებელი.*/
   }
   {/**არ მინდოდა რამე პაგინაციი library დამეყენებინა ამიტომ გავაკეთ შემდეგნაირად. */}
-  const renderPageNumbers = () => {
+   const renderPageNumbers = () => {
     const pageNumbers = [];
-    const maxPageNumbers = 5; // maximum number of page numbers to show before and after the current page
+    const maxPageNumbers = 5;
     const totalPages = Math.ceil(users.length / itemsPerPage);
-  
-   
+
     if (totalPages <= maxPageNumbers) {
       for (let i = 1; i <= totalPages; i++) {
         pageNumbers.push(
@@ -88,15 +93,19 @@ useEffect(() => {
     } else {
       let startPage = Math.max(1, currentPage - maxPageNumbers);
       let endPage = Math.min(totalPages, currentPage + maxPageNumbers);
-  
+
       if (currentPage <= maxPageNumbers) {
         endPage = maxPageNumbers * 2;
       } else if (currentPage >= totalPages - maxPageNumbers) {
         startPage = totalPages - maxPageNumbers * 2;
       }
-  
+
       for (let i = startPage; i <= endPage; i++) {
-        if (i === 1 || i === totalPages || (i >= currentPage - maxPageNumbers && i <= currentPage + maxPageNumbers)) {
+        if (
+          i === 1 ||
+          i === totalPages ||
+          (i >= currentPage - maxPageNumbers && i <= currentPage + maxPageNumbers)
+        ) {
           pageNumbers.push(
             <li
               key={i}
@@ -117,9 +126,10 @@ useEffect(() => {
         }
       }
     }
-  
+
     return pageNumbers;
   };
+
 
 
 
@@ -143,29 +153,38 @@ useEffect(() => {
     /**ეს იმიტომ გავაკეთე რომ შეგვეძლოს კოდის გამარტივება და პატარა ნაწილებად დაყოფა,ადვილად მენეჯმენტისთვის. */
   }
   {/**გადავაკეთე რომ იპოვოს ლოკაციის მიხედვით ასევე იყოს 20 ინვენტორი დაგადავიდეთ პიგნაციით. */}
-  const renderTableData = currentItems.map((user, index) => {
-    if (selectedLocation === "All" || selectedLocation === user.location) {
-    return (
-      <tr key={index}>
-        <td>{user.name}</td>
-        <td>{user.location}</td>
-        <td>{user.price}</td>
-        <td>
-          <button
-            className="btn btn-danger"
-            onClick={() => handleDeleteClick(user.id)}
-          >
-            Delete
-          </button>
-        </td>
-      </tr>
-    );
-    }
-  });
+  const filteredUsers = selectedLocation === 'All' 
+  ? users 
+  : users.filter(user => user.location === selectedLocation);
+
+const startIndex = (currentPage - 1) * itemsPerPage;
+const endIndex = startIndex + itemsPerPage;
+const displayedUsers = filteredUsers.slice(startIndex, endIndex);
+const itemsLeft =displayedUsers.length
+
+
+
+
+
+const renderTableData = displayedUsers.map((user, index) => (
+  <tr key={index}>
+    <td>{user.name}</td>
+    <td>{user.location}</td>
+    <td>{user.price}</td>
+    <td>
+      <button
+        className="btn btn-danger"
+        onClick={() => handleDeleteClick(user.id)}
+      >
+        წაშლა
+      </button>
+    </td>
+  </tr>
+));
 
   return (
     <>
-    {isLoading ? (<h1 className="d-flex align-items-center justify-content-center vh-100 fs-7">Loading...</h1>) : (
+    {isLoading ? (<Spinner/>) : (
         <div className="container">
         <div className="card">
           <div className="card-title">
@@ -216,7 +235,7 @@ useEffect(() => {
         <ul className="pagination">
           {renderPageNumbers()}
         </ul>
-        <b className=""><span className="fs-5">items left:</span>{items}</b>
+        <b className=""><span className="fs-5">დარჩენილი რაოდენობა:</span>{selectedLocation =='All' ?  items : itemsLeft}</b>
       </div>
     </div>
   </nav>
